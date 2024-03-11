1227bbd8bb50055a302109bc85f417603361fcf7
kudr.max <mdkudryashov@edu.hse.ru>
2024-03-11 14:52:27 +0300

    1H-2

--- yandex/1H.py
+++ yandex/1H.py
@@ -18,13 +18,19 @@ def foo(arr):
     # temp1 = temp - (x1 - x2) % L
     x_rel, v_rel = (x1 - x2) % L, v1 - v2
     diff = -x2
+    center_coord = (0 + diff) % L
     delta = x_rel
 
     t = 0
     if v_rel == 0:
         x_fake = delta / 2
-        t1 = abs((x_fake + diff) / v_zero)
-        t2 = abs(x_fake / v_zero)
+        v_fake = v_zero
+        # t1 = abs((x_fake + diff) / v_fake
+        if v_fake > 0:
+            t1 = abs((center_coord - x_fake) / v_fake)
+        else:
+            t1 = abs((L - abs((center_coord - x_fake))) / v_fake)
+        t2 = abs(x_fake / v_fake)
         t = min(t1, t2)
     else:
         t = abs(delta / v_rel)
