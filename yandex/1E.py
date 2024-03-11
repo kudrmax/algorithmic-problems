9d67ed1b830bdde2fc6812cbed07508034f9bdff
kudr.max <mdkudryashov@edu.hse.ru>
2024-03-11 22:57:55 +0300

    1E-2 работает, но TL

--- yandex/1E.py
+++ yandex/1E.py
@@ -7,35 +7,46 @@ n = arr[0]
 k = arr[1]
 d = arr[2]
 
-map = {}
-
-is_index_checked = {}
+flag = False
 
 for i in range(d):
-    is_index_checked[i] = False
-
-i = 0
-while i < d:
-    if i < 0:
-        n = -1
-        break
-    new_figures = []
-    if is_index_checked[i] == False:
+    if n % k == 0:
+        n = 10 * n
+        flag = True
+    else:
         for m in range(10):
-            new_num = n * 10 + m
-            if new_num % k == 0:
-                new_figures.append(m)
-        map[i] = new_figures
-    if len(map[i]) == 0:
-        is_index_checked[i] = False
-        n = n // 10
-        i -= 1
-        continue
+            new_n = n * 10 + m
+            if new_n % k == 0:
+                n = new_n
+                flag = True
+                break
+if flag:
+    print(n)
+else:
+    print(-1)
 
-    m = map[i].pop()
-    n = n * 10 + m
-    is_index_checked[i] = True
-
-    i += 1
-
-print(n)
+# i = 0
+# while i < d:
+#     if i < 0:
+#         n = -1
+#         break
+#     new_figures = []
+#     if is_index_checked[i] == False:
+#         for m in range(10):
+#             new_num = n * 10 + m
+#             if new_num % k == 0:
+#                 new_figures.append(m)
+#         map[i] = new_figures
+#     if len(map[i]) == 0:
+#         is_index_checked[i] = False
+#         n = n // 10
+#         i -= 1
+#         continue
+#
+#     m = map[i].pop()
+#     n = n * 10 + m
+#     is_index_checked[i] = True
+#
+#     i += 1
+#
+# print(n)
