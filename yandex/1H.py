d4ffc07eee9caeeaae4396c972756a963c143adf
kudr.max <mdkudryashov@edu.hse.ru>
2024-03-11 15:24:28 +0300

    1H-4 готово для ненулевых скоростей

--- yandex/1H.py
+++ yandex/1H.py
@@ -1,28 +1,26 @@
+def get_time_collision(x1, v1, x2, v2, L):
+    v_rel = v1 - v2
+    if v_rel < 0:
+        delta = (x1 - x2) % L
+    else:
+        delta = (x2 - x1) % L
+    t = delta / v_rel
+    return abs(t)
+
 def foo(arr):
     L = arr[0]
     x1, v1 = arr[1], arr[2]
     x2, v2 = arr[3], arr[4]
 
-    x, v = (x1 - x2) % L, v1 - v2
-    c = (-x2) % L
+    x2_fake = L - x2
+    v2_fake = -v2
 
-    t = 0
-
-    if v != 0:
-        p1 = 0
-        p2 = (2 * c) % L
-        delta1 = (x - p1) % L
-        delta2 = (x - p2) % L
-        # if v < 0:
-        #     delta1 = L - delta1
-        #     delta2 = L - delta2
-        t1 = abs(delta1 / v)
-        t2 = abs(delta2 / v)
+    if v1 != 0 or v2 != 0:
+        t1 = get_time_collision(x1, v1, x2, v2, L)
+        t2 = get_time_collision(x1, v1, x2_fake, v2_fake, L)
         t = min(t1, t2)
         print('YES')
-        print(abs(t))
-        return
-
+        print(t, t1, t2)
 
 f = open('input.txt', 'r')
 
