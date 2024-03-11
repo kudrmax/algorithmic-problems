a42f9a88247dbdb04c43d2da230b58ea7f653952
kudr.max <mdkudryashov@edu.hse.ru>
2024-03-11 13:44:45 +0300

    Ввел перенос СК

--- yandex/1H.py
+++ yandex/1H.py
@@ -3,8 +3,8 @@ def foo(arr):
     x1, v1 = arr[1], arr[2]
     x2, v2 = arr[3], arr[4]
 
-    x_zero, v_zero = x2, v2
-    x_rel, v_rel = x1, v1 - v2
+    x_zero, v_zero = 0, v2
+    x_rel, v_rel = x1 - x2, v1 - v2
     # x_fake, v_fake = L - x1, -v1
     # x_fake_rel, v_fake_rel = x_fake, v_fake - v2
 
@@ -15,9 +15,9 @@ def foo(arr):
     # delta = x_zero - x_fake_rel
     # t = delta / v_fake_rel
 
-    delta = x_zero - x_rel
-    if v_rel < 0:
-        delta = delta - L
+    delta = x_rel
+    # if v_rel < 0:
+    #     delta = delta - L
     t = delta / v_rel
 
     print('YES')
