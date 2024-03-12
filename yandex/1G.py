ff7efa2efbceb703c8176470ab12614b119ff8c4
kudr.max <mdkudryashov@edu.hse.ru>
2024-03-12 12:58:21 +0300

    1G-3

--- yandex/1G.py
+++ yandex/1G.py
@@ -5,19 +5,28 @@ for line in f:
 
 x = arr[0]  # мой мощь (хп и солдаты)
 y = arr[1]  # хп казармы
-p = arr[2]  # кол-во производимых солдат
+delta_p = arr[2]  # кол-во производимых солдат
+
+p = 0
 
 y -= x
+if y != 0:
+    p = delta_p
 c = 1
 
 while True:
 
+    if x <= 0:
+        c = -1
+        break
+
     if y == 0:
+        # мой ход
         if x >= p:
             c += 1
             break
         else:  # x < p
-            if x > p - x:
+            if x > delta_p - x:
                 c += 2
                 break
             else:  # x <= p - x
@@ -25,11 +34,25 @@ while True:
                 break
     else:  # y != 0
         if y > x:
-            y -= (x - p)
+            y -= (x - delta_p)
+            p = 0
         else:  # y <= x
             # тут должен быть перебор
-            y = 0
-            p -= x - y
+            # p -= x - y
+            # y = 0
+            if x >= p:
+                y -= (x - p)
+                p = 0
+            else:  # x < p
+                p -= x
+
+    if p < 0:
+        p = 0
+
+    # его ход
+    x -= p
+    if y != 0:
+        p += delta_p
 
     c += 1
 
