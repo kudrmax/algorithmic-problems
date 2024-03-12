229d27564f040285b2f3cb7cc9903676a7e17d7e
kudr.max <mdkudryashov@edu.hse.ru>
2024-03-12 14:29:21 +0300

    1G-5

--- yandex/1G.py
+++ yandex/1G.py
@@ -17,6 +17,7 @@ c = 1
 done = False
 data_list = [[x, y, p, c, done]]
 
+
 def produce_soldiers(data):
     x, y, p, c, done = data
     if not done:
@@ -29,6 +30,7 @@ def produce_soldiers(data):
             done = True
     return [x, y, p, c, done]
 
+
 def check_win(data):
     x, y, p, c, done = data
     if x <= 0:
@@ -36,27 +38,20 @@ def check_win(data):
         c = -1
     elif y <= 0 and p <= 0:
         done = True
-    elif c > 10000:
-        c = -1
-        done = True
+    # elif c > 10000:
+    #     c = -1
+    #     done = True
     return [x, y, p, c, done]
+
 
 def do_step(data):
     data = check_win(data)
     x, y, p, c, done = data
 
-
     if done:
         return [data]
-    # if x <= 0:
-    #     c = -1
-    #     return [[x, y, p, c, True]]
-    #
-    # if y <= 0 and p <= 0:
-    #     return [[x, y, p, c, True]]
 
     if y == 0:
-        # мой ход
         if x >= p:
             c += 1
             return [[x, y, p, c, True]]
@@ -69,40 +64,39 @@ def do_step(data):
                 return [[x, y, p, c, True]]
     else:  # y != 0
         if y > x:
-            y -= (x - delta_p)
+            y -= (x - p)
             p = 0
             arr = [x, y, p, c + 1, False]
-            arr = check_win(arr)
+            # arr = check_win(arr)
             arr = produce_soldiers(arr)
             arr = check_win(arr)
             return [arr]
         else:  # y <= x
             # первый вариант
-
-            # p -= x - y
-            # y = 0
-
             arr1 = [x, 0, p - (x - y), c + 1, False]
+            arr1 = check_win(arr1)
 
             # второй вариант
-            # if x >= p:
-            #     y -= (x - p)
-            #     p = 0
-            # else:  # x < p
-            #     p -= x
-            if x >= p:
-                arr2 = [x, y - (x - p), 0, c + 1, False]
-            else:
-                arr2 = [x, y, p - x, c + 1, False]
 
-            arr1= check_win(arr1)
-            arr2= check_win(arr2)
+            if p == x:
+                arr2 = [0, 1, 1, -1, True]
+            elif p > x:
+                arr2 = [0, 1, 1, -1, True]
+            else:  # p < x
+                arr2 = [x, y - (x - p), 0, c + 1, False]
+            # if x >= p:
+            #     arr2 = [x, y - (x - p), 0, c + 1, False]
+            # else:
+            #     arr2 = [x, y, p - x, c + 1, False]
+
+            arr2 = check_win(arr2)
             arr1 = produce_soldiers(arr1)
             arr2 = produce_soldiers(arr2)
-            arr1= check_win(arr1)
-            arr2= check_win(arr2)
+            arr1 = check_win(arr1)
+            arr2 = check_win(arr2)
 
             return [arr1, arr2]
+
 
 while True:
     new_data_list = []
@@ -111,8 +105,8 @@ while True:
         datas = do_step(data)
         for d in datas:
             new_data_list.append(d)
-    data_list = [list(x) for x in set(tuple(x) for x in new_data_list)]
-    # data_list = new_data_list
+    # data_list = [list(x) for x in set(tuple(x) for x in new_data_list)]
+    data_list = new_data_list
 
     count_done = 0
     for i in range(len(data_list)):
@@ -121,48 +115,6 @@ while True:
     if count_done == len(data_list):
         break
 
-#
-# while True:
-#
-#     if x <= 0:
-#         c = -1
-#         break
-#
-#     if y == 0:
-#         # мой ход
-#         if x >= p:
-#             c += 1
-#             break
-#         else:  # x < p
-#             if x > delta_p - x:
-#                 c += 2
-#                 break
-#             else:  # x <= p - x
-#                 c = -1
-#                 break
-#     else:  # y != 0
-#         if y > x:
-#             y -= (x - delta_p)
-#             p = 0
-#         else:  # y <= x
-#             # тут должен быть перебор
-#             # p -= x - y
-#             # y = 0
-#             if x >= p:
-#                 y -= (x - p)
-#                 p = 0
-#             else:  # x < p
-#                 p -= x
-#
-#     if p < 0:
-#         p = 0
-#
-#     # его ход
-#     x -= p
-#     if y != 0:
-#         p += delta_p
-#
-#     c += 1
 
 c_min = 1000000
 for i in range(len(data_list)):
