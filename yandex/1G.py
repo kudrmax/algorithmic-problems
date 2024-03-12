8527faed97d1fe6151c07d2f70810fec910b6e16
kudr.max <mdkudryashov@edu.hse.ru>
2024-03-12 12:44:44 +0300

    1G-2

--- yandex/1G.py
+++ yandex/1G.py
@@ -3,24 +3,34 @@ arr = []
 for line in f:
     arr.append(list(map(int, line.split()))[0])
 
-X = arr[0]  # мой мощь (хп и солдаты)
-Y = arr[1]  # хп казармы
-P = arr[2]  # кол-во производимых солдат
+x = arr[0]  # мой мощь (хп и солдаты)
+y = arr[1]  # хп казармы
+p = arr[2]  # кол-во производимых солдат
 
-Y -= X
-count = 1
-while X > 0 and Y + P > 0:
-    x, p = X, P
-    # мой удар
-    if Y > x:
-        p = 0
-        Y -= x - P
-    elif Y <= x:
-        Y = 0
-        p -= x
-        P = p
-    # его удар
-    X -= p
-    count += 1
+y -= x
+c = 1
 
-print(count)
+while True:
+
+    if y == 0:
+        if x >= p:
+            c += 1
+            break
+        else:  # x < p
+            if x > p - x:
+                c += 2
+                break
+            else:  # x <= p - x
+                c = -1
+                break
+    else:  # y != 0
+        if y > x:
+            y -= (x - p)
+        else:  # y <= x
+            # тут должен быть перебор
+            y = 0
+            p -= x - y
+
+    c += 1
+
+print(c)
