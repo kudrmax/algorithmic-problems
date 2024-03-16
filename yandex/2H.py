18c792c70a7c46d12c9a5ba03029cda38a8a699d
kudr.max <mdkudryashov@edu.hse.ru>
2024-03-16 14:37:42 +0300

    2H-2

--- yandex/2H.py
+++ yandex/2H.py
@@ -5,68 +5,67 @@ for line in lines[1:]:
     arr.append(list(map(int, line.split(' '))))
 # print(arr)
 
-max_el = arr[0][0]
-max_indexes_x = []
-max_indexes_y = []
+d = {}
 
+max_el = arr[0][0]
+max_index = [0, 0]
 for i in range(len(arr)):
     for j in range(len(arr[0])):
         a = arr[i][j]
-        max_el = max(max_el, a)
-# print(f'{max_el = }')
+        if a > max_el:
+            max_el = a
+            max_index = [i, j]
 
+second_max_el = -1
+second_max_index = [0, 0]
+for j in range(len(arr[0])):  # прохожусь по строке
+    i = max_index[0]
+    a = arr[i][j]
+    if a >= second_max_el:
+        if [i, j] != max_index:
+            second_max_el = a
+            second_max_index = [i, j]
+for j in range(len(arr)):  # прохожусь по столбцу
+    i = max_index[1]
+    a = arr[i][j]
+    if a >= second_max_el:
+        if [i, j] != max_index:
+            second_max_el = a
+            second_max_index = [i, j]
+
+if max_index[0] == second_max_index[0]:
+    d['row'] = max_index[0]
+else:
+    d['col'] = max_index[1]
+
+third_max_index = [0, 0]
+third_max_el = -1
 for i in range(len(arr)):
     for j in range(len(arr[0])):
-        if arr[i][j] == max_el:
-            max_indexes_y.append(i)
-            max_indexes_x.append(j)
-max_indexes_y = list(set(max_indexes_y))
-max_indexes_x = list(set(max_indexes_x))
-# print(f'{max_indexes_y = }')
-# print(f'{max_indexes_x = }')
+        a = arr[i][j]
+        if 'row' in d:
+            already_checked = i == d['row']
+        else:
+            already_checked = j == d['col']
+        if not already_checked:
+            if a > third_max_el:
+                third_max_el = a
+                third_max_index = [i, j]
 
-d_y = {}
-for max_index_y in max_indexes_y:
-    count = 0
-    for el in arr[max_index_y]:
-        if el == max_el:
-            count += 1
-    d_y[max_index_y] = count
+if 'row' in d:
+    d['col'] = third_max_index[1]
+else:
+    d['row'] = third_max_index[0]
 
-d_x = {}
-for max_index_x in max_indexes_x:
-    count = 0
-    for i in range(len(arr)):
-        el = arr[i][max_index_x]
-        if el == max_el:
-            count += 1
-    d_x[max_index_x] = count
+row = d['row'] + 1
+col = d['col'] + 1
 
-# print(f'{d_y = }')
-# print(f'{d_x = }')
+print(f'{max_el = }')
+print(f'{max_index = }')
+print(f'{second_max_el = }')
+print(f'{second_max_index = }')
+print(d)
+print(f'{third_max_el = }')
+print(f'{third_max_index = }')
 
-max_val_y = 0
-for key, val in d_y.items():
-    max_val_y = max(max_val_y, val)
-# print(f'{max_val_y = }')
-
-max_val_x = 0
-for kex, val in d_x.items():
-    max_val_x = max(max_val_x, val)
-# print(f'{max_val_x = }')
-
-max_key_y = 0
-for key, val in d_y.items():
-    if val == max_val_y:
-        max_key_y = key
-        break
-# print(f'{max_key_y = }')
-
-max_key_x = 0
-for key, val in d_x.items():
-    if val == max_val_x:
-        max_key_x = key
-        break
-# print(f'{max_key_x = }')
-
-print(f'{max_key_y + 1} {max_key_x + 1}')
+print(f'{row} {col}')
