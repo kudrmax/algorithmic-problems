d5d47b9fc5c97a491560b3114d57554c89e95e95
kudr.max <mdkudryashov@edu.hse.ru>
2024-03-19 17:40:13 +0300

    2I-6

--- yandex/2I.py
+++ yandex/2I.py
@@ -29,20 +29,26 @@ def calc_dist(cursor_i, cursor_j, i, j):
     dist = abs(delta_i) + abs(delta_j)
     return dist
 
+col_list = [k for k in range(N)]
 
+# res_list = []
+min_res = 1000000000
+for col in col_list:
+    i, j = 0, col
+    cursor_i = 0
+    res = 0
+    while True:
+        if [i, j] in arr:
+            dist = calc_dist(cursor_i, col, i ,j)
+            res += dist
+            cursor_i += 1
 
-col = 1
-i, j = 0, col
-cursor_i = 0
-res = 0
-while True:
-    if [i, j] in arr:
-        dist = calc_dist(cursor_i, col, i ,j)
-        res += dist
-        cursor_i += 1
+        i, j, flag = move_sursor(i, j, col, N - 1, N - 1)
+        if not flag:
+            break
+    min_res = min(min_res, res)
+    # res_list.append(res)
+    # print(res)
 
-    i, j, flag = move_sursor(i, j, col, N - 1, N - 1)
-    if not flag:
-        break
-
-print(res)
+print(min_res)
+# print(min(res_list))
