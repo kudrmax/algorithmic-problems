1f6230d1d3d9b2af81c3550c2d95c3dad2491568
kudr.max <mdkudryashov@edu.hse.ru>
2024-03-19 17:35:07 +0300

    2I-5

--- yandex/2I.py
+++ yandex/2I.py
@@ -6,7 +6,7 @@ for line in lines[1:]:
 N = len(arr)
 for i in range(len(arr)):
     arr[i] = [arr[i][0] - 1, arr[i][1] - 1]
-print(arr, N)
+# print(arr, N)
 
 def move_sursor(i, j, zero_j, max_i, max_j):
     if j <= zero_j:
@@ -23,13 +23,26 @@ def move_sursor(i, j, zero_j, max_i, max_j):
     return i, j, True
 
 
+def calc_dist(cursor_i, cursor_j, i, j):
+    delta_i = cursor_i - i
+    delta_j = cursor_j - j
+    dist = abs(delta_i) + abs(delta_j)
+    return dist
+
+
+
 col = 1
 i, j = 0, col
-print(i, j)
+cursor_i = 0
+res = 0
 while True:
+    if [i, j] in arr:
+        dist = calc_dist(cursor_i, col, i ,j)
+        res += dist
+        cursor_i += 1
+
     i, j, flag = move_sursor(i, j, col, N - 1, N - 1)
-    print(i, j)
     if not flag:
         break
-    # if [i, j] in arr:
-    #     print(i, j)
+
+print(res)
