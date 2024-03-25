8964aaafc2d43bb67a08e2e0007dda500a632d4d
kudr.max <mdkudryashov@edu.hse.ru>
2024-03-25 16:52:44 +0300

    3H-2

--- yandex/3H.py
+++ yandex/3H.py
@@ -1,3 +1,4 @@
+import collections
 import math
 
 with open('input.txt', 'r') as file:
@@ -13,8 +14,8 @@ print(f'{N = }')
 print(f'{A = }')
 print(f'{B = }')
 
-A_set = set()
-B_set = set()
+A_set = collections.defaultdict(int)
+B_set = collections.defaultdict(int)
 
 def get_angle(s):
     x1, y1, x2, y2 = s
@@ -23,13 +24,20 @@ def get_angle(s):
     return math.atan((y2 - y1) / (x2 - x1))
 
 for i in range(len(A)):
-    A_set.add(get_angle(A[i]))
-    B_set.add(get_angle(B[i]))
+    A_set[get_angle(A[i])] += 1
+    B_set[get_angle(B[i])] += 1
 
 print(A_set)
 print(B_set)
 
-print(A_set.symmetric_difference(B_set))
+count = 0
+for key_a, val_a in A_set.items():
+    val_b = B_set[key_a]
+    count += abs(val_b - val_a)
+
+print(count)
+
+# print(A_set.symmetric_difference(B_set))
 
 # for i in range(len(A)):
 #     a = A[i]
