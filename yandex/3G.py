384ba95badb89ba12912f1e1bc76ba4493063d8c
kudr.max <mdkudryashov@edu.hse.ru>
2024-03-25 13:36:48 +0300

    3G-2

--- yandex/3G.py
+++ yandex/3G.py
@@ -1,18 +1,17 @@
 def foo():
     with open('input.txt', 'r') as file:
         lines = file.readlines()
-
     N = int(lines[0].split()[0])
     arr = []
     for line in lines[1:]:
         arr.append(tuple(map(int, line.split())))
+    s = set(arr)
+    # print(N, arr, s)
 
-    print(N, arr)
+    if N <= 1:
+        return 3
 
-    if N == 1:
-        return 1
-
-    arr = [(0, 0), (2, 2)]
+    res = 2
 
     for i in range(len(arr)):
         for j in range(i + 1, len(arr)):
@@ -25,6 +24,16 @@ def foo():
             b1 = (round(0.5 * ((x1 + x2) + (y2 - y1))), round(0.5 * ((y1 + y2) + (x1 - x2))))
             b2 = (round(0.5 * ((x1 + x2) - (y2 - y1))), round(0.5 * ((y1 + y2) - (x1 - x2))))
 
-            print(a1, a2, b1, b2)
+            if b1 in s and b2 in s:
+                return 0
 
-foo()
+            if b1 in s and b2 not in s:
+                res = 1
+
+            if b2 in s and b1 not in s:
+                res = 1
+
+            # print(a1, a2, b1, b2)
+    return res
+
+print(foo())
