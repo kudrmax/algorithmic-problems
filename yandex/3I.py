fc09677e06fdf9a415656ce604bb01f90fbeb1ce
kudr.max <mdkudryashov@edu.hse.ru>
2024-03-26 12:29:24 +0300

    3I-4

--- yandex/3I.py
+++ yandex/3I.py
@@ -18,84 +18,90 @@ with open('input.txt', 'r') as file:
 commands = []
 for command in lines:
     commands.append(command.strip())
+commands.reverse()
 
 players = collections.defaultdict(list)
 teams = collections.defaultdict(lambda: collections.defaultdict(int))
 
 
-def add_data_to_players(name: str, match_id, time):
+def add_data_to_players(name: str, time, match_id):
     score = {'match_id': match_id, 'time': time}
     players[name].append(score)
 
 
-add_data_to_players('Max', 0, 10)
-add_data_to_players('Vika', 1, 20)
-
-teams['HSE']['score_sum'] += 1
-teams['HSE']['count_of_matches'] += 1
-teams['HSE']['count_first_scores'] += 1
-teams['HSE']['count_first_scores'] += 1
+# teams['HSE']['score_sum'] += 1
+# teams['HSE']['count_of_matches'] += 1
+# teams['HSE']['count_first_scores'] += 1
+# teams['HSE']['count_first_scores'] += 1
 
 
 print(players)
 print(teams)
 
-# for command in commands:
-#
-#     match = match_score_pattern.match(command)
-#     if match:
-#         data.team1 = match.group(1)
-#         data.team2 = match.group(2)
-#         data.score1 = int(match.group(3))
-#         data.score2 = int(match.group(4))
-#         data.score1_remain = data.score1
-#         data.score2_remain = data.score2
-#         print(f"{data.team1} - {data.team2} {data.score1}:{data.score2}")
-#
-#     match = goal_pattern.match(command)
-#     if match:
-#         if data.score1_remain > 0:
-#             data.scores1.append({
-#                 'name': match.group(1),
-#                 'score': int(match.group(2))
-#             })
-#             data.score1_remain -= 1
-#         else:
-#             pass
-#         print(f"{match.group(1)} {match.group(2)}")
-#
-#     match = total_goals_for_pattern.match(command)
-#     if match:
-#         print(f"Total goals for '{match.group(1)}'")
-#
-#     match = mean_goals_per_game_for_pattern.match(command)
-#     if match:
-#         print(f"Mean goals per game for {match.group(1)}")
-#
-#     match = total_goals_by_pattern.match(command)
-#     if match:
-#         print(f"Total goals by {match.group(1)}")
-#
-#     match = mean_goals_per_game_by_pattern.match(command)
-#     if match:
-#         print(f"Mean goals per game by {match.group(1)}")
-#
-#     match = goals_on_minute_pattern.match(command)
-#     if match:
-#         print(f"Goals on minute {match.group(1)} by {match.group(2)}")
-#
-#     match = goals_on_first_t_minutes_pattern.match(command)
-#     if match:
-#         print(f"Goals on first {match.group(1)} minutes by {match.group(2)}")
-#
-#     match = goals_on_last_t_minutes_pattern.match(command)
-#     if match:
-#         print(f"Goals on last {match.group(1)} minutes by {match.group(2)}")
-#
-#     match = score_opens_by_team_pattern.match(command)
-#     if match:
-#         print(f"Score opens by {match.group(1)}")
-#
-#     match = score_opens_by_player_pattern.match(command)
-#     if match:
-#         print(f"Score opens by {match.group(1)}")
+while len(commands) > 0:
+    command = commands.pop()
+
+    match = match_score_pattern.match(command)
+    if match:
+        team1_name = match.group(1)
+        team2_name = match.group(2)
+        score1 = int(match.group(3))
+        score2 = int(match.group(4))
+        match_id = f"{team1_name} - {team2_name} {score1}:{score2}"
+        print(f"Done: {team1_name} - {team2_name} {score1}:{score2}")
+
+        for i in range(score1 + score2):
+            command = commands.pop()
+            match = goal_pattern.match(command)
+            if match:
+                player_name = match.group(1)
+                time = match.group(2)
+                add_data_to_players(name=player_name, time=time, match_id=match_id)
+                print(f"Done: {player_name} {time}")
+
+    # match = total_goals_for_pattern.match(command)
+    # if match:
+    #     player_name = match.group(1)
+    #     print(f"Total goals for '{player_name}'")
+
+    # match = mean_goals_per_game_for_pattern.match(command)
+    # if match:
+    #     print(f"Mean goals per game for {match.group(1)}")
+    #
+    match = total_goals_by_pattern.match(command)
+    if match:
+        player_name = match.group(1)
+        scores = players[player_name]
+        s = set()
+        for score in scores:
+            s.add(score['match_id'])
+        match_count = len(s)
+        score_count = len(scores)
+        total_goals = score_count / match_count
+        print(f"Done: Total goals by {player_name} ---> {total_goals}")
+    #
+    # match = mean_goals_per_game_by_pattern.match(command)
+    # if match:
+    #     print(f"Mean goals per game by {match.group(1)}")
+    #
+    # match = goals_on_minute_pattern.match(command)
+    # if match:
+    #     print(f"Goals on minute {match.group(1)} by {match.group(2)}")
+    #
+    # match = goals_on_first_t_minutes_pattern.match(command)
+    # if match:
+    #     print(f"Goals on first {match.group(1)} minutes by {match.group(2)}")
+    #
+    # match = goals_on_last_t_minutes_pattern.match(command)
+    # if match:
+    #     print(f"Goals on last {match.group(1)} minutes by {match.group(2)}")
+    #
+    # match = score_opens_by_team_pattern.match(command)
+    # if match:
+    #     print(f"Score opens by {match.group(1)}")
+    #
+    # match = score_opens_by_player_pattern.match(command)
+    # if match:
+    #     print(f"Score opens by {match.group(1)}")
+
+print(players)
