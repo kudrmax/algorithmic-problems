bd5c006c9698d526399acae1f7d0bea78834c674
kudr.max <mdkudryashov@edu.hse.ru>
2024-03-26 12:44:29 +0300

    3I-7

--- yandex/3I.py
+++ yandex/3I.py
@@ -49,6 +49,8 @@ while len(commands) > 0:
         score2 = int(match.group(4))
         match_id = f"{team1_name} - {team2_name} {score1}:{score2}"
 
+        print(f"Done: {team1_name} - {team2_name} {score1}:{score2}")
+
         time1, time2 = 0, 0
         for i in range(score1 + score2):
             command = commands.pop()
@@ -71,18 +73,20 @@ while len(commands) > 0:
         else:
             teams[team2_name]['count_first_scores'] += 1
 
-        print(f"Done: {team1_name} - {team2_name} {score1}:{score2}")
-
     match = total_goals_for_pattern.match(command)
     if match:
         team_name = match.group(1)
         total_goals = teams[team_name]['score_sum']
         print(f"Done: Total goals for '{team_name}' ---> {total_goals}")
 
-    # match = mean_goals_per_game_for_pattern.match(command)
-    # if match:
-    #     print(f"Mean goals per game for {match.group(1)}")
-    #
+    match = mean_goals_per_game_for_pattern.match(command)
+    if match:
+        team_name = match.group(1)
+        score_count = teams[team_name]['score_sum']
+        match_count = teams[team_name]['count_of_matches']
+        mean_goals = score_count / match_count if match_count != 0 else 0
+        print(f"Done: Mean goals per game for {team_name} ---> {mean_goals}")
+
     match = total_goals_by_pattern.match(command)
     if match:
         player_name = match.group(1)
@@ -101,25 +105,25 @@ while len(commands) > 0:
         score_count = len(scores)
         mean_goals = score_count / match_count if match_count != 0 else 0
         print(f"Done: Mean goals per game by {player_name} ---> {mean_goals}")
-    #
-    # match = goals_on_minute_pattern.match(command)
-    # if match:
-    #     print(f"Goals on minute {match.group(1)} by {match.group(2)}")
-    #
-    # match = goals_on_first_t_minutes_pattern.match(command)
-    # if match:
-    #     print(f"Goals on first {match.group(1)} minutes by {match.group(2)}")
-    #
-    # match = goals_on_last_t_minutes_pattern.match(command)
-    # if match:
-    #     print(f"Goals on last {match.group(1)} minutes by {match.group(2)}")
-    #
-    # match = score_opens_by_team_pattern.match(command)
-    # if match:
-    #     print(f"Score opens by {match.group(1)}")
-    #
-    # match = score_opens_by_player_pattern.match(command)
-    # if match:
-    #     print(f"Score opens by {match.group(1)}")
+
+    match = goals_on_minute_pattern.match(command)
+    if match:
+        print(f"Goals on minute {match.group(1)} by {match.group(2)}")
+
+    match = goals_on_first_t_minutes_pattern.match(command)
+    if match:
+        print(f"Goals on first {match.group(1)} minutes by {match.group(2)}")
+
+    match = goals_on_last_t_minutes_pattern.match(command)
+    if match:
+        print(f"Goals on last {match.group(1)} minutes by {match.group(2)}")
+
+    match = score_opens_by_team_pattern.match(command)
+    if match:
+        print(f"Score opens by {match.group(1)}")
+
+    match = score_opens_by_player_pattern.match(command)
+    if match:
+        print(f"Score opens by {match.group(1)}")
 
 print(players)
