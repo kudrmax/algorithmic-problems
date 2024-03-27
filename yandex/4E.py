bf48060ae987539ef5d9710ab91f43b457e39473
kudr.max <mdkudryashov@edu.hse.ru>
2024-03-27 13:40:53 +0300

    4E-2

--- yandex/4E.py
+++ yandex/4E.py
@@ -3,10 +3,13 @@ import math
 with open('input.txt', 'r') as file:
     lines = file.readlines()
 n = list(map(int, lines[0].split()))[0]
-print(f'{n = }')
+# print(f'{n = }')
 
 
 def get_N(n):
+    # temp1 = 1 + 8 * n
+    # temp = math.sqrt(1 + 8 * n)
+    # N = (-1 + 2 * math.sqrt(1 + 8 * n)) // 4
     N = -(3 / 2) + 1 / 2 * math.sqrt(1 + 8 * n)
     if N % 1 != 0:
         N = int(N // 1 + 1)
@@ -34,8 +37,14 @@ def bs(s1, s2, target):
 N = get_N(n)
 s1 = get_sum(N) + 1
 s2 = get_sum(N + 1)
-print(f'{N = }')
-print(f'{s1 = }, {s2 = }')
+if s1 > n:
+    N -= 1
+elif s2 < n:
+    N += 1
+s1 = get_sum(N) + 1
+s2 = get_sum(N + 1)
+# print(f'{N = }')
+# print(f'{s1 = }, {s2 = }')
 
 # index = bs(s1, s2, n)
 # print(f'{index = }')
@@ -43,4 +52,8 @@ print(f'{s1 = }, {s2 = }')
 # index = n - s1 + 1
 # print(f'{index = }')
 
-print(f'{s2 - n + 1}/{n - s1 + 1}')
+i1 = s2 - n + 1
+i2 = n - s1 + 1
+if N % 2 == 0:
+    i1, i2 = i2, i1
+print(f'{i1}/{i2}')
