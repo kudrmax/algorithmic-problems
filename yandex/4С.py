18494783746a23b9efb32a2fceb20df7b5669d52
kudr.max <mdkudryashov@edu.hse.ru>
2024-03-27 12:35:00 +0300

    4C-1

--- yandex/4С.py
+++ yandex/4С.py
@@ -8,12 +8,35 @@ for line in lines[2:]:
     pair = tuple(map(int, line.split()))
     pairs.append(pair)
 
-print(arr)
-print(pairs)
-
 prefix_sums = [_ for _ in range(len(arr))]
 prefix_sums[-1] = 0
 for i in range(len(arr)):
     prefix_sums[i] = prefix_sums[i - 1] + arr[i]
 
-print(prefix_sums)
+
+def bs(arr, prefix_sums, l, s):
+    left, right = 0, len(arr) - 1
+    while left < right:
+        mid = (left + right) // 2
+        delta = prefix_sums[mid] - prefix_sums[mid - l] if mid - l >= 0 else prefix_sums[mid]
+        if delta == s:
+            return mid
+        elif delta < s:
+            left = mid + 1
+        else:
+            right = mid
+    if prefix_sums[left] == s:
+        return left
+    return -1
+
+# print(arr)
+# print(pairs)
+# print(prefix_sums)
+
+for l, s in pairs:
+    t = bs(arr, prefix_sums, l, s)
+    if t == -1:
+        print(-1)
+    # print(t, t - l + 1)
+    else:
+        print(t - l + 1 + 1)
