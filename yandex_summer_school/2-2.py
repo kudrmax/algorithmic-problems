88fe092c7f20e3e5c0c0788ce8190abfc36b3bbb
kudr.max <mdkudryashov@edu.hse.ru>
2024-04-21 20:02:06 +0300

    2.2 5

--- yandex_summer_school/2-2.py
+++ yandex_summer_school/2-2.py
@@ -6,8 +6,10 @@ n, k = tuple(map(int, lines[0].strip().split()))
 prices = list(map(int, lines[1].strip().split()))
 
 # res = [0 for _ in range(n)]
+# res = [0 for _ in range(n)]
 res = []
 
+flag = False
 dp = [0 for _ in range(n)]
 dp[-1] = prices[-1]
 dp.append(0)
@@ -19,19 +21,26 @@ for i in range(n - 2, -1, -1):
         if var <= min_price:
             min_price = var
             min_j = j
+            if j == n:
+                flag = True
     dp[i] = min_price
-    for m in range(min_j - i):
-        res.append(0)
+    # for m in range(min_j - i):
+    #     res.append(0)
     res.append(min_j)
 res.append(0)
 
 res = list(reversed(res))
+if not flag:
+    res.append(n)
 
-# print(dp)
+print(dp[0])
+print(dp)
 print(res)
-# print(dp[0])
 for a, b in zip(res[:-1], res[1:]):
     print(b - a, end=' ')
+    print('0' * (b - a - 1), end='')
+    if '0' * (b - a - 1) != '':
+        print(' ', end='')
 
 
 # print(*list(), sep=' ')
