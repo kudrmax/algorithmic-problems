8fe758e0c3010cb51c5943b1159a01751a1ffedc
kudr.max <mdkudryashov@edu.hse.ru>
2024-04-21 18:42:30 +0300

    2.1 3

--- yandex_summer_school/2-1.py
+++ yandex_summer_school/2-1.py
@@ -15,7 +15,13 @@ def foo():
 
     step_counter = 0
 
+    win_counter = 0
+    first_win = False
+    second_win = False
+
     for x, y in arr:
+        step_counter += 1
+
         ##### hy
         if y not in hy:
             hy[y] = {}
@@ -31,8 +37,12 @@ def foo():
             j += 1
         for k in range(x - j + 1, x + i):
             hy[y][k] = max_counter + 1
-        if max_counter + 1 == 5:
-            return step_counter
+        if max_counter + 1 >= 5:
+            win_counter += 1
+            if step_counter % 2 != 0:
+                first_win = True
+            else:
+                second_win = True
 
         ##### hx
         if x not in hx:
@@ -49,31 +59,21 @@ def foo():
             j += 1
         for k in range(y - j + 1, y + i):
             hx[x][k] = max_counter + 1
-        if max_counter + 1 == 5:
-            return step_counter
+        if max_counter + 1 >= 5:
+            win_counter += 1
+            if step_counter % 2 != 0:
+                first_win = True
+            else:
+                second_win = True
 
-    # for x, y in arr:
-    #     step_counter += 1
-    #     for h, key_fixed, key_changing in zip([hy, hx], [y, x], [x, y]):
-    #         h[key_fixed] = 1
-    #         max_counter = 1
-    #         i = 0
-    #         while key_changing + i in h[key_fixed]:
-    #             max_counter += 1
-    #             i += 1
-    #         j = 0
-    #         while key_changing - j in h:
-    #             max_counter = max(max_counter, h[key_changing - j])
-    #             j += 1
-    #         for k in range(key_changing - j + 1, key_changing + i):
-    #             h[key_fixed][k] = max_counter
-    #         if max_counter == 5:
-    #             return step_counter
-    # return step_counter
+    if win_counter == 0:
+        return 'Draw'
+    if win_counter == 1:
+        if first_win:
+            return 'First'
+        if second_win:
+            return 'Second'
+    return 'Inattention'
 
 
-step_counter = foo()
-if step_counter % 2 == 0:
-    print('First')
-else:
-    print('Second')
+print(foo())
