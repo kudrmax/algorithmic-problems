a00f2103c20cd3f5952329979e4d29ed67d4a702
kudr.max <mdkudryashov@edu.hse.ru>
2024-05-26 01:44:06 +0300

    roc_auc 6 (final)

--- yandex_ml_studcamp/expression.py
+++ yandex_ml_studcamp/expression.py
@@ -8,10 +8,10 @@ def roc_auc(probabilities, y_true):
 
     # сортировка данных по probabilities по возрастанию
     sorted_indexes = np.argsort(y_proba)[::-1]
-    # y_proba = y_proba[sorted_indexes]
+    y_proba = y_proba[sorted_indexes]
     y_true = y_true[sorted_indexes]
 
-    roc_auc_value: np.float64 = np.float64(0.0)  # итоговая площадь
+    roc_auc_value = 0  # итоговая площадь
     FPR_prev = 0  # понадобится для вычисления площади
     for i in range(len(y_true) + 1):  # подсчет ROC_AUC
         # i - порог
@@ -20,32 +20,26 @@ def roc_auc(probabilities, y_true):
         # считаем по определению TP, FP, FN, TN при пороге = i
         TP, FP, FN, TN = 0, 0, 0, 0
         for j in range(len(y_true)):
-            if j < i and y_true[j] == 1:
+            if j <= i and y_true[j] == 1:
                 TP += 1
-            if j >= i and y_true[j] == 0:
+            if j > i and y_true[j] == 0:
                 TN += 1
-            if j < i and y_true[j] == 0:
+            if j <= i and y_true[j] == 0:
                 FP += 1
-            if j >= i and y_true[j] == 1:
+            if j > i and y_true[j] == 1:
                 FN += 1
 
         # считаем по определению TPR, FPR при пороге = i
-
-        if TP + FN == 0:
-            pass
-        if FP + TN == 0:
-            pass
-
         TPR = TP / (TP + FN)
         FPR = FP / (FP + TN)
 
         # считаем площадь
-        if FP != 0:  # тоже самое, что и "FPR != 0"
+        if FPR != 0:
             # если точка ROC-кривой сдвинулась вправо по оси FPR,
             # то добавим площадь, которая образуется за счет этого сдвига, в итоговый результат
-            x: np.float64 = np.float64(FPR - FPR_prev)  # сдвиг по оси x (FPR)
-            y: np.float64 = np.float64(TPR)  # координата по оси y (TPR)
-            delta_s: np.float64 = np.float64(x * y)  # площадь
+            x = FPR - FPR_prev  # сдвиг по оси x (FPR)
+            y = TPR  # координата по оси y (TPR)
+            delta_s = x * y  # площадь
             roc_auc_value += delta_s
 
         FPR_prev = FPR
